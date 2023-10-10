a879e89481b000b57c39a1d02d1ca4d452a49956
sbsampaio <samuel.sampaio@edu.ufes.br>
2023-10-10 17:33:11 -0300

    Resolução TAD_04

--- 04_TAD_simples/TAD_04/Respostas/SamuelBagatelli/main.c
+++ 04_TAD_simples/TAD_04/Respostas/SamuelBagatelli/main.c
@@ -1,7 +1,48 @@
 #include <stdio.h>
+#include "aluno.h"
+
+void OrganizaPorMatricula(int tam, tAluno vetor[tam]);
 
 int main(int argc, char const *argv[])
 {
+    int i;
+    int numAlunos;
+
+    scanf("%d", &numAlunos);
+
+    tAluno vetAlunos[numAlunos];
+
+    for (i = 0; i < numAlunos; i++)
+    {
+        vetAlunos[i] = LeAluno();
+    }
+
+    OrganizaPorMatricula(numAlunos, vetAlunos);
+
+    for (i = 0; i < numAlunos; i++)
+    {
+        if (VerificaAprovacao(vetAlunos[i]))
+            ImprimeAluno(vetAlunos[i]);
+    }
 
     return 0;
+}
+
+void OrganizaPorMatricula(int tam, tAluno vetor[tam])
+{
+    int k, j;
+    tAluno aux;
+
+    for (k = tam - 1; k > 0; k--)
+    {
+        for (j = 0; j < k; j++)
+        {
+            if (ComparaMatricula(vetor[j], vetor[j + 1]) == 1)
+            {
+                aux = vetor[j];
+                vetor[j] = vetor[j + 1];
+                vetor[j + 1] = aux;
+            }
+        }
+    }
 }
